1e1179ebae5c141c054fee07d38cb80977129f9c
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-10-29 18:51:27 +0200

    Main completo

--- src/main/java/com/mandelbrot/conjuntoMandelbrot/ConjuntoMandelbrotApplication.java
+++ src/main/java/com/mandelbrot/conjuntoMandelbrot/ConjuntoMandelbrotApplication.java
@@ -10,8 +10,18 @@ import javax.swing.*;
 public class ConjuntoMandelbrotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConjuntoMandelbrotApplication.class, args);
 
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(MandelbrotPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
 
+		java.awt.EventQueue.invokeLater(() -> new MandelbrotPanel().setVisible(true));
 	}
 }
