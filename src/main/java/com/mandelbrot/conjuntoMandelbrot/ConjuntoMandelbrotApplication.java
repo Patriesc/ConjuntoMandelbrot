b6ef4823b4d449d43f0f216665c201edc36667bd
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-10-29 18:37:29 +0200

    Proyecto creado con Spring Boot y estructurado

---
+++ src/main/java/com/mandelbrot/conjuntoMandelbrot/ConjuntoMandelbrotApplication.java
@@ -0,0 +1,17 @@
+package com.mandelbrot.conjuntoMandelbrot;
+
+import com.mandelbrot.conjuntoMandelbrot.vistas.MandelbrotPanel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ConjuntoMandelbrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjuntoMandelbrotApplication.class, args);
+
+
+	}
+}
